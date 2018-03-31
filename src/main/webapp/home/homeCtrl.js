a3f52f8f11bb25517bc379c0a160fb50c0dbc2ef
Nikola Dakic <nikoladakic@uns.ac.rs>
2018-03-31 23:10:11 +0200

    bla1

--- src/main/webapp/home/homeCtrl.js
+++ src/main/webapp/home/homeCtrl.js
@@ -5,30 +5,76 @@
 
 (function (angular) {
     angular.module('HCIApp')
-        .controller('homeCtrl', function($scope){
+        .controller('homeCtrl', function($scope, $http){
             var vm = this;
 
-            $scope.items = [];
+            //$scope.items = [0];
 
-            // var loadData = function () {
-            //     var promise = $http.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=0P5MHVJ1YM8H62BG&outputsize=compact");
-            //     promise.then(function (response) {
-            //
-            //         $scope.items = angular.fromJson((response.data["Time Series (Daily)"]));
-            //
-            //         console.log($scope.items);
-            //     });
-            // };
-            //
-            // loadData();
+            var loadData = function () {
+                var promise = $http.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=0P5MHVJ1YM8H62BG&outputsize=compact");
+                promise.then(function (response) {
+
+                    $scope.items = response.data["Time Series (Daily)"];
+
+                    $scope.dates = [];
+                    $scope.opens = [];
+                    $scope.highs = [];
+                    $scope.lows = [];
+
+
+                    $scope.sadrzaj = [];
+
+                    var count = 0;
+
+                    for(var date in response.data["Time Series (Daily)"]){
+
+                        if(count < 10){
+
+                            $scope.dates.push(date);
+
+                            for(var info in response.data["Time Series (Daily)"][date]){
+
+                                if(info == "1. open"){
+                                    $scope.opens.push(response.data["Time Series (Daily)"][date][info]);
+
+                                    var podatak = {
+
+                                        x: date,
+                                        y: response.data["Time Series (Daily)"][date][info]
+
+                                    };
+
+                                    $scope.sadrzaj.push(podatak);
+
+                                };
+                                if(info == "2. high"){
+                                    $scope.highs.push(response.data["Time Series (Daily)"][date][info]);
+                                };
+                                if(info == "3. low"){
+                                    $scope.lows.push(response.data["Time Series (Daily)"][date][info]);
+                                };
+                            };
+
+                        };
+
+                        count++;
+                    };
+
+
+                });
+            };
+
+            loadData();
 
 
             $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
             $scope.series = ['Series A', 'Series B'];
+
             $scope.data = [
-                [65, 59, 80, 81, 56, 55, 40],
-                [28, 48, 40, 19, 86, 27, 90]
+                [65, 59, 80, 81, 211, 55, 40],
+                [28, 48, 40, 19, 86, 27, 210]
             ];
+
             $scope.onClick = function (points, evt) {
                 console.log(points, evt);
             };
@@ -53,7 +99,7 @@
             };
 
             $scope.labels2 = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-            $scope.data2 = [300, 500, 100]
+            $scope.data2 = [300, 500, 100];
 
             $scope.labels3 =["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"];
 
@@ -61,7 +107,6 @@
                 [65, 59, 90, 81, 56, 55, 40],
                 [28, 48, 40, 19, 96, 27, 100]
             ];
-
 
         });
 }(angular));
